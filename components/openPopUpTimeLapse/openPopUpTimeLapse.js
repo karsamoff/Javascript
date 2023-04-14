3c65bdd77b49cabec4b2b64472b4f5f5681ec64d
karsamoff <130576117+karsamoff@users.noreply.github.com>
2023-04-14 12:00:24 -0500

    analisisPorLinea

--- components/openPopUpTimeLapse/openPopUpTimeLapse.js
+++ components/openPopUpTimeLapse/openPopUpTimeLapse.js
@@ -1,9 +1,9 @@
- function abrir_ventana()
+ function abrir_ventana()/* ésta es la funcion para abrir ventanas */
  {
     window.open("http://www.eltiempo.com/");
- }
- function para_abrir()
+ }/* this funtion open the window  */
+ function parar_abrir()
  {
     clearTimeout(pop);
- }
- pop = setInterval ("abrir_ventana()",3000);
+ }/* ésta funcion para de abrir la ventana lo usamos en el btn of the page */
+ pop = setInterval ("abrir_ventana()",3000);/* ésta función abre la ventana cada cierto tiempo */
